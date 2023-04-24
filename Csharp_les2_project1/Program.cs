1deb32750660a7f133fcef78ddd39890cfc486e8
leonid <leonidsleo@yandex.ru>
2023-04-24 09:59:00 +0300

    создал проект к задачи 1 лекции 2, находим наиб из 9 с помощью стандартных команд

---
+++ Csharp_les2_project1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Решение задач на уроке2 по навыкам программирования. Язык C#.
+// Найти максимальное число из 9
+// Вариант решения без массива.
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 43;
+int b1 = 77;
+int c1 = 12;
+int a2 = 431;
+int b2 = 717;
+int c2 = 112;
+int a3 = 4351;
+int b3 = 7743;
+int c3 = 1112;
+
+int max = Max(Max(a1, b1,c1), Max(a2, b2,c2), Max(a3, b3,c3));
+Console.Write("Наибольшее число: ");
+Console.WriteLine(max);
