e732dca1ec7e52cc84a431bae7c33137d0662496
leonid <leonidsleo@yandex.ru>
2023-04-25 21:44:33 +0300

    создана программа к дз_10 семинара 2

--- C_sharp_dom2_zad10/Program.cs
+++ C_sharp_dom2_zad10/Program.cs
@@ -4,3 +4,10 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Показать вторую цифру трехзначного числа!");
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int a1 = a / 10; int a2 = a1 % 10;
+Console.WriteLine($"Второй цифрой числа {a} является цифра {a2}");
+Console.WriteLine("Программа завершена");
